b66730d47f26e1f870c23cf6bde55338ac39db97
Freddy Ramirez <firf.1818@gmail.com>
2022-07-28 23:20:18 -0400

    terminado fase 1 falta agregar la version mobile y mas cositas pero hay vamos

---
+++ src/js/index.js
@@ -0,0 +1,75 @@
+let listFood = document.querySelectorAll('.list-food li');
+let body = document.querySelector('body')
+
+listFood.forEach((list) =>{
+    list.addEventListener('mouseenter',backgroundChange)
+})
+
+function backgroundChange(e){
+
+    // console.log(e.target.parentNode)
+
+    switch(e.target.id){
+        case 'Vegetable':
+            toChange(e,e.target.id);
+            break;
+        case 'Apple':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Orange':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Watermelon':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Mango':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Carrot':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Onion':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Tomato':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Potato':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Meat':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Chicken':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Fish':
+            toChange(e,e.target.id);
+            break;
+
+        case 'Egg':
+            toChange(e,e.target.id);
+            break;
+    }
+
+}
+
+const toChange = (e,name)=>{
+    // console.log(e.target.parentNode)
+    // console.log(name)
+
+    // console.log(`url('./img/${name}.jpg')`)
+
+    e.target.parentNode.style.backgroundImage = `url('./img/${name}.jpg')`;
+
+}

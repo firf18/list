1ad1d6c40ca8901b3ef477f703fafb1c4911d189
Freddy Ramirez <firf.1818@gmail.com>
2022-07-29 01:06:08 -0400

    skad

--- js/index.js
+++ js/index.js
@@ -1,70 +1,70 @@
 let listFood = document.querySelectorAll('.list-food li');
 let body = document.querySelector('body')
 
-listFood.forEach((list) =>{
-    list.addEventListener('mouseenter',backgroundChange)
+listFood.forEach((list) => {
+    list.addEventListener('mouseenter', backgroundChange)
 })
 
-function backgroundChange(e){
+function backgroundChange  (e) {
 
     // console.log(e.target.parentNode)
 
-    switch(e.target.id){
+    switch (e.target.id) {
         case 'Vegetable':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
         case 'Apple':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Orange':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Watermelon':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Mango':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Carrot':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Onion':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Tomato':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Potato':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'carne':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Chicken':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Fish':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
 
         case 'Egg':
-            toChange(e,e.target.id);
+            toChange(e, e.target.id);
             break;
     }
 
 }
 
-const toChange = (e,name)=>{
+const toChange = (e, name) => {
     // console.log(e.target.parentNode)
     // console.log(name)
 
